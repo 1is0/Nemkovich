00244a7e04fdb37a3cdb3387c56df9fd86e6b824
Monerd <33130058+Monerd@users.noreply.github.com>
2020-05-12 14:37:14 +0300

    Add files via upload

---
+++ Labs-CS/5 Lab/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Lab_5___Csh__
+{
+    class Program
+    {
+        public static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Error input. Repeat, please: ");
+            }
+            return a;
+        }
+        public static bool CheckBool()
+        {
+            bool a = false;
+            while (!bool.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Error, input again: ");
+            }
+            return a;
+        }
+        public static string Check()
+        {
+            string a;
+            a = Console.ReadLine();
+            while (a.Length > 20)
+            {
+                Console.WriteLine("Error input. Repeat, please: ");
+                a = Console.ReadLine();
+            }
+            return a;
+        }
+        public static CarPark CreateCarPark()
+        {
+            Console.WriteLine("Enter basic information: ");
+            Console.WriteLine("Quantity of cars: ");
+            int Cars = CheckInt();
+            Console.WriteLine("Quantity of trucks: ");
+            int Trucks = CheckInt();
+            Console.WriteLine("Quantity of places to relax: ");
+            int PlacesToRelax = CheckInt();
+            Console.WriteLine("Quantity of manholes: ");
+            int Manholes = CheckInt();
+            Console.WriteLine("Quantity of service stations: ");
+            int ServiceStations = CheckInt();
+            bool magnetic_parking = false;
+            Console.WriteLine("Is there a magnetic parking lot? True ( yes ) or False ( no )");
+            magnetic_parking = CheckBool();
+            Console.WriteLine("Enter quantity of equipments on special vehicles: ");
+            int Amount = CheckInt();
+            TownCarPark CarPark = new TownCarPark(Cars, Trucks, ServiceStations, Manholes, PlacesToRelax, magnetic_parking, Amount);
+            if (Amount > 0)
+            {
+                Console.WriteLine("Enter items through 'Enter' in turn: ");
+                for (int i = 0; i < Amount; i++)
+                {
+                    CarPark[i] = Check();
+                }
+            }
+            Console.Clear();
+            return CarPark;
+        }
+        public static void CarPark_List(CarPark[] List)
+        {
+            for (int i = 0; i < List.Length; i++)
+            {
+                List[i] = CreateCarPark();
+            }
+            for (int i = 0; i < List.Length; i++)
+            {
+                Console.WriteLine();
+                Console.Write(i + 1 + ")"); List[i].Information();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int Amount_New = 0;
+            TownCarPark first = new TownCarPark();
+            TownCarPark second = new TownCarPark(5, 6, 3);
+            TownCarPark third = new TownCarPark(2, 3, 1, 2, 5, true, 0);
+            var fourth = CreateCarPark();
+            first.Information();
+            second.Information();
+            third.Information();
+            fourth.Information();
+            Console.WriteLine("How many car parks do you want to add? ");
+            Amount_New = CheckInt();
+            TownCarPark[] List = new TownCarPark[Amount_New];
+            CarPark_List(List);
+            Console.WriteLine($"Number of car parks created: {TownCarPark.Quantity_Car_Park}");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ Labs-CS/5 Lab/TownCarPark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_5___Csh__
+{
+    class TownCarPark : VillageCarPark
+    {
+        private readonly bool magnetic_parking;
+        public TownCarPark() : base() { }
+        public TownCarPark(int _equipments) : base(_equipments) { }
+        public TownCarPark(int _cars, int _trucks, int _equipments) : base(_cars, _trucks, _equipments) { }
+        public TownCarPark(int _cars, int _trucks, int _service_stations, int _manholes, int _places_to_relax, bool _magnetic_parking, int _equipments) : base(_cars, _trucks, _equipments)
+        {
+            ServiceStations = _service_stations;
+            Manholes = _manholes;
+            PlacesToRelax = _places_to_relax;
+            magnetic_parking = _magnetic_parking;
+        }
+        public override void Information()
+        {
+            base.Information();
+            if (magnetic_parking)
+                Console.WriteLine("Car Park with new Magnetic Parking.");
+            else
+                Console.WriteLine("Car Park without new Magnetic Parking.");
+            Console.WriteLine();
+        }
+
+    }
+}
