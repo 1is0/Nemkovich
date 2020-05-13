df40e9e7629c323d2ab058013b3c08ca15d94791
Monerd <33130058+Monerd@users.noreply.github.com>
2020-05-13 11:12:59 +0300

    Add files via upload

---
+++ Labs-CS/4 Lab/1 Part/Mp3Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4___Csh__
+{
+    class Mp3Player : IDisposable
+    {
+        public Mp3Player(string fileName)
+        {
+            const string form = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string Command = String.Format(form, fileName);
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void Play()
+        {
+            string Command = "play MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void Pause()
+        {
+            string Command = "pause MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void Stop()
+        {
+            string Command = "close MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void SetVolume(int volume)
+        {
+            string command = "setaudio MediaFile volume to " + (volume * 100).ToString();
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+        public void Dispose()
+        {
+            string Command = "close MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+    }
+}
